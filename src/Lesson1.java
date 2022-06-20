8eaa261a80c35ff04d99635b9cfa3b8d5e35d4c1
idknow94 <i.dont.kei.94@gmail.com>
2022-06-20 22:13:15 +0400

    added folder's contents

---
+++ src/Lesson1.java
@@ -0,0 +1,8 @@
+public class Lesson1 {
+    public static void main(String[] args) {
+        String s = "Hello";
+        int i = 15;
+        long l = 1546565465L;
+        System.out.println(s + ' ' + i + ' ' + l);
+    }
+}
